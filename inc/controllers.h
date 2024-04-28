3ceb72044a8a24a4f1c25b471e39d4b8340bc890
seyednasermoravej <seyednasermoravej@gmail.com>
2024-04-28 08:19:05 +0330

    controller added.

---
+++ inc/controllers.h
@@ -0,0 +1,52 @@
+#ifndef CONTROLLERS_H_
+#define CONTROLLERS_H_
+
+#include <stdint.h>
+
+
+/**
+ * Library written by ALEK GUEDEGBE for Workflow PSIM to IMPERIX
+ * PI controller library
+ * Definition and operation according to the Workflow PSIM to IMPERIX
+ */
+
+
+typedef struct{
+	float kp,ti;				// Gain and time constant of PI
+	float limup;				// Upper saturation value of the output
+	float limlow;				// Lower saturation value of the output
+	float ui_prev;				// Previous value of the integral component
+	float e_prev;				// Previous value of the error
+    	float Fsamp;                // sampling frequency of the PI controller
+	float A1 ;
+
+} PIController;
+
+
+
+/**
+ * Routine to configure the PI controller 'me' and pre-compute the necessary constants.
+ * @param *me		the PID pseudo-object to be configured
+ * @param kp		proportional gain
+ * @param ti		integral time constant
+ * @param limup		upper saturation threshold of the output quantity
+ * @param limlow	lower saturation threshold of the output quantity
+ * @param Fsamp 	sampling (interrupt) frequency
+ * @return void
+ */
+
+void ConfigPIController(PIController* me, float kp, float ti, float limup, float limlow, float Fsamp);
+
+
+
+/**
+ * Routines to run the pseudo-object 'me' depending of its actual nature (PI, PID, etc. controllers)
+ * @param *me		the corresponding PID pseudo-object (parameters and state quantities)
+ * @param error		the setpoint value minus the measured value
+ * @return			the control variable for the measured quantity (output of the controller)
+ */
+
+float RunPIController(PIController* me, float error);
+
+
+#endif /*CONTROLLERS_H_*/
---
+++ src/controllers.c
@@ -0,0 +1,84 @@
+/*
+ *	@title	Discrete closed-loop controllers
+ *	@author	ALEK GUEDEGBE (LAEH CEA GRENOBLE)
+ *	@file	controllers.c <--> WPI=Workflow PSIM to IMPERIX
+ */
+
+
+#include "controllers.h"					                                    // Corresponding header file
+#include <math.h>							                                    // Standard math library
+
+#include "Core/core.h"
+
+
+/*
+ * Routine to configure the PI controller and pre-compute the necessary constants.
+ */
+
+void ConfigPIController(PIController* me, float kp, float ti, float limup, float limlow, float Fsamp)
+{
+	// Set the controller parameters:
+	me->kp = kp;
+	me->ti = ti;
+	me->limup = limup;
+	me->limlow = limlow;
+	me->Fsamp = Fsamp;
+
+    me->A1 = kp / (ti*Fsamp);
+
+	// Initialize the state quantities:
+
+	me->e_prev = 0.0;
+	me->ui_prev = 0.0;
+}
+
+
+
+
+/*
+ * Routine to run the PI controller 'me' .
+ */
+
+float RunPIController(PIController* me, float error)
+{
+	float up;
+	float ui;							                                    // Integral part of the output
+	float u;
+
+
+	up= me->kp * error;
+	ui = ( me->A1 * error) + (me->ui_prev) ;
+
+	u = up + ui ;
+
+	if ( (u>= me->limup) || (u <= me->limlow) )
+	{
+		ui =  me->ui_prev;
+	}
+	else
+	{
+		ui = ( me->A1 * error) + (me->ui_prev) ;
+	}
+
+
+
+//Stores sample [K] in variables [K-1] in order to be used in the next sampling instant
+	me->e_prev = error;
+	me->ui_prev = ui;
+
+//Limiter at the output of the compensator
+	if (u>= me->limup) {
+		u = me->limup;
+	}
+
+	if (u <= me->limlow) {
+		u = me->limlow;
+	}
+
+
+	// Reset the integral when the outputs are inhibited (when the converter is blocked):
+	if(GetCoreState() != OPERATING)
+		me->ui_prev = 0.0;					                                    // Avoid integrating when core has been disabled
+
+	return u;
+}
